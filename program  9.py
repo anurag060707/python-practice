57a5c80bf6375589d31e129da9beb88863535ba4
anurag060707 <anuragsharma03062007@gmail.com>
2026-06-23 15:42:47 +0530

    Add files via upload

---
+++ program  9.py
@@ -0,0 +1,7 @@
+num1=int(input("enter first number:"))
+num2=int(input("enter second number:"))
+num3=num1
+num1=num2
+num2=num3
+#after swaping two number
+print("first number=",num1,"second number",num2)
