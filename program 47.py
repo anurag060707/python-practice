f6998ec0954b2c7fa1555955ae9c8c9f666d729c
anurag060707 <anuragsharma03062007@gmail.com>
2026-06-24 11:54:19 +0530

    Add files via upload

---
+++ program 47.py
@@ -0,0 +1,8 @@
+# a Python program to check whether a person is a senior citizen (age 60 or above).
+age=int(input("enter age:"))
+if(age>=60):
+    print("a person is a senior citizen")
+else:
+    print("a person is not a senior citizen")    
+
+
