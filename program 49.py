5458204febc0156d4aa801eb7ce6784732974b71
anurag060707 <anuragsharma03062007@gmail.com>
2026-06-24 12:23:39 +0530

    Add files via upload

---
+++ program 49.py
@@ -0,0 +1,6 @@
+# a Python program to check whether a number lies within the range 1–100. 
+num=int(input("enter number:"))
+if(num>=1 and num<=100):
+    print("number lies within the range 1-100")
+else:
+    print("number does not lie within range 1-100")    
