ec783e43890b708e38b08fdbab3d9baff90d2449
anurag060707 <anuragsharma03062007@gmail.com>
2026-06-24 08:56:17 +0530

    Add files via upload

---
+++ program  42.py
@@ -0,0 +1,41 @@
+# a Python program to determine the month name based on the month number (1–12). 
+print("choose 1-7 to print month name")
+print("1 -january")
+print("2- febuary")
+print("3-march")
+print("4-april")
+print("5-may")
+print("6-june")
+print("7-july")
+print("8-august")
+print("9-september")
+print("10-october")
+print("11-november")
+print("12-december")
+choice=int(input("enter your choice:"))
+if(choice==1):
+    print("january")
+elif(choice==2):
+    print("febuary")
+elif(choice==3):
+    print("march")
+elif(choice==4):
+    print("april")
+elif(choice==5):
+    print("may")
+elif(choice==6):
+    print("june")
+elif(choice==7):
+    print("july")
+elif(choice==8):
+    print("august")
+elif(choice==9):
+    print("september")
+elif(choice==10):
+    print("october")
+elif(choice==11):
+    print("november")
+elif(choice==12):
+    print("december")  
+else:
+    print("wrong choice")                                              
