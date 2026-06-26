f6df1d9d7332be280aead3ba10b2d16b4f4f6821
anurag060707 <anuragsharma03062007@gmail.com>
2026-06-26 10:42:28 +0530

    Add files via upload

---
+++ program 69.py
@@ -0,0 +1,7 @@
+#a Python program to print the following pattern:#a Python program to print the following pattern:
+ 
+ 
+for i in range(1,6):
+    for j in range(1,i+1):
+        print("*",end=" ")
+    print()    
