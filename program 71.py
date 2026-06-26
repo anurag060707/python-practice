b10115c8df15b66228b262ad2550f80433fbb7b8
anurag060707 <anuragsharma03062007@gmail.com>
2026-06-26 14:52:37 +0530

    Add files via upload

---
+++ program 71.py
@@ -0,0 +1,8 @@
+#Write a Python program to print a pyramid pattern: 
+for i in range(1,6):
+    for j in range(1,10):
+        if(j>=6-i and j<=4+i):
+            print("*",end="")
+        else:
+            print(" ",end="")
+    print()            
