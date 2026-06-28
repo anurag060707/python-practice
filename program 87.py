50e55e01aeded63897633cdf9964b776e4ebe54e
anurag060707 <anuragsharma03062007@gmail.com>
2026-06-28 16:37:34 +0530

    Add files via upload

---
+++ program 87.py
@@ -0,0 +1,7 @@
+#a Python program to check whether a substring exists in a string. 
+string=input("enter string:")
+substring=input("enter substring:")
+if substring in string:
+    print("substring exists:")
+else:
+    print("substring does not exists:")    
