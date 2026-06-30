5c9b6dcfd6cdc9e1daa6989002a6a3f0d7068d27
anurag060707 <anuragsharma03062007@gmail.com>
2026-06-30 20:53:12 +0530

    Add files via upload

---
+++ program 111.py
@@ -0,0 +1,7 @@
+# a Python program to create a set and display its elements.
+a=set()
+n=int(input("enter length of set:"))
+for i in range(n):
+    x=int(input("enter elements:"))
+    a.add(x)
+print(a)    
