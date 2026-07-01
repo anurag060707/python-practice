5ed7a8b97743af5dd6d233e822e091a7cbaac566
anurag060707 <anuragsharma03062007@gmail.com>
2026-07-01 17:06:23 +0530

    Add files via upload

---
+++ program 121.py
@@ -0,0 +1,7 @@
+# a Python program to create a dictionary containing student information such as name,age, and marks.
+student={}
+student["name"]=(input("enter name:"))
+student["age"]=int(input("enter age:"))
+student["marks"]=int(input("enter marks:"))
+print("student information")
+print(student)
