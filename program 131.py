a4406da263a375b698ec4d777c703e0f6f3d04d5
anurag060707 <anuragsharma03062007@gmail.com>
2026-07-02 08:43:32 +0530

    Add files via upload

---
+++ program 131.py
@@ -0,0 +1,6 @@
+# a Python function to add two numbers and return the result. 
+def add(num1,num2):
+    sum=num1+num2
+    return sum
+result=add(3,4)
+print("after adition of two number=",result)  
